c3e754e0b81cb5c7f5e118f336234715b5ef2e99
nineno98 <marcell.kovacs123@gmail.com>
2025-11-21 16:04:32 +0100

    üzenetküldés működik

--- response_files/controllers/textController.js
+++ response_files/controllers/textController.js
@@ -2,12 +2,11 @@ const db = require('../db/connection');
 
 exports.sendText = (req, res) => {
     const text = req.body.usertext;
-
     db.query("INSERT INTO texts (content) VALUES (?)", [text], (err, result) => {
         if(err){
             throw err;
         }
         console.log("Text inserted:", text);
-        res.redirect("/send");
-    })
+        res.json({"message":"success!"})
+    });
 }
