ce716524dc915ea0e106e96ecab020643aeadbc2
Marcell Kovács <marcell.kovacs123@gmail.com>
2025-12-11 10:33:57 +0100

    emial regex vol 1

--- controllers/resetpassController.js
+++ controllers/resetpassController.js
@@ -2,9 +2,14 @@ const db = require("../db/connection")
 
 
 exports.resetPassword = (req, res) => {
+
     const email = req.body.email;
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const isValidEmail = (email) => emailRegex.test(email);
     
-    console.log(email);
+    if(isValidEmail(email)){
+        console.log('valid');
+    }
     res.json(
                 {
                     "status":"success",
